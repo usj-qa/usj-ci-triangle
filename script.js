6dfe1ae388ab00b3d32e35ad5508408cc10b16f6
dcoloma-usj <dcoloma@usj.es>
2026-04-22 18:19:06 +0200

    Introducing a bug on purpose by not returning SCALENE

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function getTriangleType(lengthA, lengthB, lengthC) {
       } else if ( (a==b) || (b==c) || (a==c)) {
         type = ISOSCELES;
       } else {
-        type = SCALENE;
+        type = ISOSCELES;
       }
     }
   }
